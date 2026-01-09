1104eeb6c5f0fe0f73103c637139462ae6fa7273
Julian_Glass-Pilon <julian@astucemedia.com>
2026-01-09 11:51:16 -0500

    Imported base website files

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1767912134',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/',
+      
+        '/professional-case-studies/',
+      
+        '/personal-projects/',
+      
+        '/contact-me/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
